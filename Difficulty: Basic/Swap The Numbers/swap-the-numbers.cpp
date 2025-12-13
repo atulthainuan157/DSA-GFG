109f5264081170b76eb85eaea371a08c2fa67838
Atul Thainuan <atulthainuan157@gmail.com>
2025-12-13 18:58:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
+++ Difficulty: Basic/Swap The Numbers/swap-the-numbers.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a, b;
-    cin >> a >> b;
-    swap(a,b);
-    cout << a << " " << b << endl;
-    return 0;
+void swap(int &a, int &b) {
+    // code here
+    int temp=a;
+    a=b;
+    b=temp;
 }
