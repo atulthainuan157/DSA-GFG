866a70cb995ca6ceb5d20d6302c609f1f4866f1f
Atul Thainuan <atulthainuan157@gmail.com>
2026-05-23 20:05:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Transform to Sum Tree/transform-to-sum-tree.cpp
+++ Difficulty: Easy/Transform to Sum Tree/transform-to-sum-tree.cpp
@@ -15,20 +15,18 @@ public:
 
 class Solution {
   public:
-    int transformTree(Node* root) {
-        if(root==NULL) {
-            return 0;
-        }
-        int leftSum = transformTree(root->left);
-        int rightSum = transformTree(root->right);
-        int oldValue = root->data;
-        root->data = leftSum + rightSum;
-        return root->data + oldValue;
-    }
+  int transformTree(Node* root) {
+      if(root == NULL) {
+          return 0;
+      }
+      int leftSum = transformTree(root->left);
+      int rightSum = transformTree(root->right);
+      int oldVal = root->data;
+      root->data = leftSum + rightSum;
+      return root->data + oldVal;
+  }
+  
     void toSumTree(Node *root) {
-        if(root == NULL) {
-            return;
-        }
         transformTree(root);
     }
 };
