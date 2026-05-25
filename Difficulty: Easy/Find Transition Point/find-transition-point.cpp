7bb05c9e1e3acc203319a085cfd44535451484b9
Atul Thainuan <atulthainuan157@gmail.com>
2026-05-25 12:51:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Transition Point/find-transition-point.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int transitionPoint(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == 1) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+    return ans;
+    }
+};
